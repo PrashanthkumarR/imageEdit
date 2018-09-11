24146c5f44c8c53e64cb1abadf1d336888b368b3
PrashanthkumarR <prashanthninu@gmail.com>
2018-09-11 14:21:46 +0530

    image cropper is done using cropper.js

--- imageCropperApp/src/app/app.component.ts
+++ imageCropperApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var Cropper: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imageCropperApp';
+  title = 'imagecropper';
+  uploadedPic: any;
+  cropper: any;
+  rotationAmount = 0;
+  onFileSelected(event) {
+    let result = document.querySelector('.result');
+    let save = document.querySelector('.save');
+    let img_w = document.querySelector('.img-w');
+    let img_h = document.querySelector('.img-h')
+    let options = document.querySelector('.options');
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.uploadedPic = e.target.result;
+      let img = document.createElement('img');
+      img.id = 'image';
+      img.src = e.target.result;
+      result.innerHTML = '';
+      result.appendChild(img);
+    
+      save.classList.remove('hide');
+      options.classList.remove('hide')
+       this.cropper =new Cropper(img);
+
+
+    }
+    reader.readAsDataURL(event.target.files[0])
+  }
+  
+  onSave(event) {
+    let imgSrc = this.cropper.getCroppedCanvas({
+      width: 5000 // input value
+    }).toDataURL();
+    let cropped = document.querySelector('.cropped');
+    let img_result = document.querySelector('.img-result');
+    cropped.classList.remove('hide');
+    img_result.classList.remove('hide');
+    this.uploadedPic = imgSrc;
+    let dwn = document.querySelector('.download')
+    dwn.classList.remove('hide');
+    dwn.setAttribute('href', imgSrc);
+  }
+  
+
+  // rotateImage(direction) {
+  //   if (direction === 'left') {
+  //     this.rotationAmount = this.rotationAmount + -45;
+  //   } else if(direction === 'right') {
+  //     this.rotationAmount = this.rotationAmount + 45;
+  //   }
+  //    (document.querySelector('.cropper-canvas') as HTMLElement).style.marginLeft = '19%';
+  //    (document.querySelector('.cropper-canvas') as HTMLElement).style.transform = 'rotate(' + this.rotationAmount + 'deg)';
+  // }
+
+  // OnUpload() {
+  //   console.log(this.uploadedPic);
+  // }
 }
